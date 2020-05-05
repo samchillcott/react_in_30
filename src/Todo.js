aca528c88b2b63895f126faf4ed8bf34df109fd4
Sam Chillcott <samchillcott@gmail.com>
2020-05-05 11:30:04 +0100

    final tut commit

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-function Todo({ todo }) {
-	return <div>{todo.name}</div>;
+function Todo({ todo, toggleTodo }) {
+	function handleTodoClick() {
+		toggleTodo(todo.id);
+	}
+	return (
+		<div>
+			<label>
+				<input
+					type="checkbox"
+					checked={todo.complete}
+					onChange={handleTodoClick}
+				/>
+				{todo.name}
+			</label>
+		</div>
+	);
 }
 
 export default Todo;
- 
